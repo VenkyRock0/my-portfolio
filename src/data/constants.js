6600fa898f861848254b744c7b19b73921b8337e
Venkatesh Babu A B <vbabu9900@gmail.com>
2023-12-30 09:09:59 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,155 @@
+export const Bio = {
+  name: "Venkatesh Babu A B",
+  roles: ["Front End Developer", "Web Developer", "Programmer", "Gamer"],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/VenkyRock0",
+  resume:
+    "https://drive.google.com/file/d/1rJp_TIZM_6JPAUryzYTah32A0Sv9bRfs/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/venkatesh-babu-0249a7153/",
+  twitter: "https://twitter.com/",
+  insta: "https://www.instagram.com/venkyrock0/",
+  facebook: "https://www.facebook.com/VenkyRock0",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    school: "CMR Institute of Technology, Bangalore, Karnataka, India",
+    date: "Nov 2020 - Oct 2022",
+    grade: "8.1 CGPA",
+    desc: "I am completed my Master's degree in CMR Institute of Technology at Bangalore. I have completed 4 semesters and have a CGPA of 8.1. I have taken courses in HTML, CSS, Javascript and Bootstrap, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Master of Computer Application",
+  },
+  {
+    id: 1,
+    school: "SRS First Grade College, Chitradurga, Karnataka, India",
+    date: "July 2017 - Sept 2020",
+    grade: "7.45 CGPA",
+    desc: "I am completed my Bachelor's degree in SRS First Grade College at Chitradurga, Karnataka, India. I have completed 4 semesters and have a CGPA of 7.45. I have taken courses in Front-end, Web Design, Java, Javascript, Object-Oriented Programming and Operating Systems among others.",
+    degree: "Bachelor's of Computer Application",
+  },
+  {
+    id: 2,
+    school: "St. Aloysius PU College, Harihar, Karnataka, India ",
+    date: "June 2015 - June 2027",
+    grade: "61.16 %",
+    desc: "I completed my class 12 high school education at SSt. Aloysius PU College, Harihar, where I studied Science with Computer Science.",
+    degree: "(XII), Science with Computer",
+  },
+  {
+    id: 3,
+    school: "Vasavi Education Society, Ajjampura, Karnataka, India",
+    date: "July 2014 - Apr 2015",
+    grade: "68.96 %",
+    desc: "I completed my class 10 education at Vasavi School, Ajjampuraa, where I studied Science, Mathematics and Socialogy.",
+    degree: "(X), Science, Mathematics and Socialogy",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Online Food Order",
+    date: "Mar 2023 - Apr 2023",
+    description:
+      "It is a responsive website which provides users to get the food to their desired location from their favourite restaurants by just sitting at home.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-demo-e9ec7.appspot.com/o/Screenshot%20(57).png?alt=media&token=07f63629-0a31-4d8e-8450-b3b08c63f78c",
+    tags: ["HTML", "CSS", "Javascript", "Bootstrap", "React Js"],
+    category: "web app",
+    github: "https://github.com/VenkyRock0/Online-Food-Order-using-React",
+  },
+  {
+    id: 0,
+    title: "Expense Tracker",
+    date: "Apr 2023 - May 2023",
+    description:
+      "Based on their salary users can upload monthly expenses and they can view expense chart monthly basis. They can also view old data by selecting year in the menu.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/react-demo-e9ec7.appspot.com/o/Screenshot%20(54).png?alt=media&token=7781c667-2e13-44cb-ba50-7193bd83b515",
+    tags: ["React Js", "Redux", "HTML", "CSS", "Javascript"],
+    category: "web app",
+    github: "https://github.com/VenkyRock0/Expense-Tracker-using-React-",
+  },
+  {
+    id: 1,
+    title: "Investment Calculator",
+    date: "Jan 2023",
+    description:
+      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/investment-calculator-cf51b.appspot.com/o/Screenshot%20(51).png?alt=media&token=38855257-96e5-4e40-b5f6-3ac76146e2a8",
+    tags: ["React Js", "Javascript", "CSS", "HTML"],
+    category: "web app",
+    github: "https://github.com/VenkyRock0/Investment-Calculator",
+  },
+];
